85c4a1d0adf4ec9943e480a30d8711b1ac51e353
Anusat07 <limpirat@gmail.com>
2026-08-23 02:43:40 +0700

    fix: publish sheets sync config for github pages

---
+++ google-sheets-config.js
@@ -0,0 +1,3 @@
+// Local-only configuration. This file is ignored by Git.
+window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbwMU-57z4izOU5vozbM4sAoV2sfvqkifa0pGQIv7-Ys4_wyG_V2nlpdmnjnmDoebGdg/exec';
+window.GOOGLE_SHEETS_SYNC_TOKEN = 'f269e32f78187948e65f92272a6446c4b250133471c406e8cb50423ef02fe8c0';
