f69a045ab08305348803f476148890bf78618e13
Anusat07 <limpirat@gmail.com>
2026-08-23 11:45:47 +0700

    use latest Apps Script deployment for sheet sync

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,3 @@
 // Local-only configuration. This file is ignored by Git.
-window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyn2sscRLxBTFFtOLobaXUL_w80sXDglccnqv9F8JJ4e6CogTJlx61_NRcD5zrct9Ni/exec';
+window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyNxzztKHSVZMnXugYSm39ApDwyxQdQRRXBXPXNQXqi6KpRuq0j6JNq_RiwKXuS5A/exec';
 window.GOOGLE_SHEETS_SYNC_TOKEN = 'f269e32f78187948e65f92272a6446c4b250133471c406e8cb50423ef02fe8c0';
