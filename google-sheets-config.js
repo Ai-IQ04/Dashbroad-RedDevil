6bd75883c0cdf994929e1368880a9a3e8683f08c
Anusat07 <limpirat@gmail.com>
2026-08-23 12:00:13 +0700

    use latest sheet cleanup deployment

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,3 @@
 // Local-only configuration. This file is ignored by Git.
-window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyNxzztKHSVZMnXugYSm39ApDwyxQdQRRXBXPXNQXqi6KpRuq0j6JNq_RiwKXuS5A/exec';
+window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycby74oklqDmjYdjJu9mljqfDZGBdbQ-ZExj7fs6l_meCTt7mM5TBIUZqZrE-QCpUxlN0/exec';
 window.GOOGLE_SHEETS_SYNC_TOKEN = 'f269e32f78187948e65f92272a6446c4b250133471c406e8cb50423ef02fe8c0';
