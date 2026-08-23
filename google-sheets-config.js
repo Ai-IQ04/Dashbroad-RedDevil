549415286c89e7335ea80e17ce6b9689b7a1f1d6
Anusat07 <limpirat@gmail.com>
2026-08-23 09:05:16 +0700

    point sheets sync to latest web app deployment

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,3 @@
 // Local-only configuration. This file is ignored by Git.
-window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbyvemJVkKLkhvzo2CCE9fIi9bF2Xu3yyOpmUFYnXMW69tmqSjGeRw3ygRzNBuvoiWfi/exec';
+window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbwd5EJK_Rsn5oK4zb3dErrdPW6_DJrLjmiGd8-oKhOXG8QzswHfvvLjm0gQB1u11-w3/exec';
 window.GOOGLE_SHEETS_SYNC_TOKEN = 'f269e32f78187948e65f92272a6446c4b250133471c406e8cb50423ef02fe8c0';
