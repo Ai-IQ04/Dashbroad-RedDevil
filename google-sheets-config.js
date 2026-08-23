fa0cacbafd2db0beaca7672644f0de47745938ef
Anusat07 <limpirat@gmail.com>
2026-08-23 10:43:08 +0700

    point sheets sync to latest deployment

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,3 @@
 // Local-only configuration. This file is ignored by Git.
-window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbwSoiUoB5edFvW7L7aWn-FkQNEan0FXsEMP0Y5xm5WN6E_Zr6Jm4rPHlpSQgZKXs0-Q/exec';
+window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbxqHyH3omMIX0fMBcjm2hUxhA6wR37EYWpdm7_HWJdyO06NWHK9mYA6XYRbJD4UGUlZ/exec';
 window.GOOGLE_SHEETS_SYNC_TOKEN = 'f269e32f78187948e65f92272a6446c4b250133471c406e8cb50423ef02fe8c0';
