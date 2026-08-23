9b3e7abc8a2ed8c22305a4a9fa37d56381699629
Anusat07 <limpirat@gmail.com>
2026-08-23 09:13:28 +0700

    update Google Sheets deployment URL

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,3 @@
 // Local-only configuration. This file is ignored by Git.
-window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbwd5EJK_Rsn5oK4zb3dErrdPW6_DJrLjmiGd8-oKhOXG8QzswHfvvLjm0gQB1u11-w3/exec';
+window.GOOGLE_SHEETS_SYNC_URL = 'https://script.google.com/macros/s/AKfycbwSoiUoB5edFvW7L7aWn-FkQNEan0FXsEMP0Y5xm5WN6E_Zr6Jm4rPHlpSQgZKXs0-Q/exec';
 window.GOOGLE_SHEETS_SYNC_TOKEN = 'f269e32f78187948e65f92272a6446c4b250133471c406e8cb50423ef02fe8c0';
